9409efd971bf97c6afbfdba7420bc7daaacf2bc3
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-03-04 16:32:59 +0900

    rm : myapp

---
+++ my-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WRvPoV1uuTZ+BAW+Qsyw34XQw8xQRs+90qvGB4cqJCg=\"\n}"
--- my-app/next.config.mjs
+++
@@ -1,6 +0,0 @@
-// next.jsの設定ファイル
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
