e03d04760a0779119f364b94fc7505d1754e88a4
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-03-08 16:53:22 +0900

    headタグの設定を編集

--- portfolio-starter-kit/pages/_document.js
+++ portfolio-starter-kit/pages/_document.js
@@ -4,13 +4,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Sasaki Subaru - Portfolio Starter Kit',
+    title: 'Sasaki Subaru - Portfolio',
     description: 'this blog based on portfolio-starter-kit created by Sasaki Subaru.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
@@ -19,7 +19,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@susasaki04" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
