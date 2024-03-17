937dd122823cd9bc1e1efc04a31fd47239fa5e7d
Subaru Sasaki <subarunrun0812@yahoo.co.jp>
2024-03-17 15:09:20 +0900

    mdxを表示できた

---
+++ my-app/app/mdx/page.tsx
@@ -0,0 +1,9 @@
+import MdxContent from "./page.mdx";
+
+export default async function Page() {
+  return (
+    <article>
+      <MdxContent />
+    </article>
+  )
+}
--- my-app/pages/markdown.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Markdown() {
-    return <div>markdown</div>
-  }
